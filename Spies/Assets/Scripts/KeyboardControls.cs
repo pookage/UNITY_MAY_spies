a5a47bda71aec0fc92b3735b4a40f17dac91763a
Pookage <pookagehayes@gmail.com>
2018-05-20 13:41:03 +0100

    added keyboard controls; just figuring out to separate the controls and movement components or not...

---
+++ Spies/Assets/Scripts/KeyboardControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControls : MonoBehaviour {
+
+	public string hand;
+
+	KeyCode leftKey;
+	KeyCode rightKey;
+
+	void Start(){
+
+		bool leftHand  = hand == "Left";
+		bool rightHand = hand == "Right";
+
+		//left key
+		if(leftHand)  leftKey = KeyCode.A;
+		if(rightHand) leftKey = KeyCode.J;
+
+		//right key
+		if(leftHand)  rightKey = KeyCode.D;
+		if(rightHand) rightKey = KeyCode.L;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(leftKey))  MoveLeft();
+		if(Input.GetKeyDown(rightKey)) MoveRight();
+	}
+
+	void MoveLeft(){
+		Debug.Log("Move left");
+	}
+
+	void MoveRight(){
+		Debug.Log("Move right");
+	}
+
+}
